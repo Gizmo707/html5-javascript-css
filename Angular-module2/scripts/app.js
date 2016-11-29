2da644fb963d38815d10c8ceb0d9f2026c47f0b1
gizmo707 <gizmopepper707@gmail.com>
2016-11-28 22:31:19 -0400

    Final solution

--- Angular-module2/scripts/app.js
+++ Angular-module2/scripts/app.js
@@ -9,32 +9,54 @@
   // Items to buy controller
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
   function ToBuyController(ShoppingListCheckOffService) {
+    var toBuy = this;
 
+    toBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+
+
+    toBuy.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buyItem(itemIndex);
+    };
+
+    toBuy.emptyToBuy = function () {
+      return ShoppingListCheckOffService.emptyToBuy();
+    }
+    toBuy.itemName = '';
+    toBuy.itemQuantity = '';
+
+    toBuy.addItem = function () {
+      ShoppingListCheckOffService.additem(toBuy.itemName, toBuy.itemQuantity);
+    }
   }
 
-  // items already bougth controller
+  // bougth items controller
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-  function AlreadyBoughtController() {
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var bought = this;
 
+    bought.items = ShoppingListCheckOffService.getItemsBought();
+    bought.emptyBought = function () {
+      return ShoppingListCheckOffService.emptyBought();
+    }
   }
 
   function ShoppingListCheckOffService() {
     var service = this;
 
     var itemsToBuy = [
-      {name: 'bags of cookies', quantity: 3},
-      {name: 'bags of chips', quantity: 5},
+      {name: 'bags of cookies       ', quantity: 3},
+      {name: 'bags of chips         ', quantity: 5},
       {name: 'bottles of soft drinks', quantity: 2},
-      {name: 'bottle of hard drink',quantity: 1},
-      {name: 'pepperoni frozen pizza',quantity: 1},
-      {name: 'bottles of antiacid', quantity: 2}
+      {name: 'bottle of hard drink  ' ,quantity: 1},
+      {name: 'pepperoni frozen pizza', quantity: 1},
+      {name: 'bottles of antiacid   ', quantity: 2}
     ];
 
-    var itemsBougth = [];
+    var itemsBought = [];
 
     service.additem = function (itemName, quantity) {
       var item = {
-        name: itemname,
+        name: itemName,
         quantity: quantity
       };
       itemsToBuy.push(item);
@@ -44,16 +66,22 @@
       return itemsToBuy;
     };
 
-    service.getItemsBougth = function () {
-      return itemsBougth;
+    service.getItemsBought = function () {
+      return itemsBought;
     };
 
     service.buyItem = function (itemIndex) {
       var item = itemsToBuy[itemIndex];
       itemsToBuy.splice(itemIndex,1);
-      itemsBougth.push(item);
+      itemsBought.push(item);
     };
 
+    service.emptyToBuy = function () {
+      return itemsToBuy.length === 0;
+    };
+
+    service.emptyBought = function () {
+      return itemsBought.length === 0;
+    };
   }
-}
-)();
+})();
